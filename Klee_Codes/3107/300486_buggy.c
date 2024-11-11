414a27fb2abcf36863aa35159444a4135c35dff2
Muyeed Ahmed <akib100095@gmail.com>
2024-11-11 01:45:15 -0500

    check - server

--- Klee_Codes/3107/300486_buggy.c
+++ Klee_Codes/3107/300486_buggy.c
@@ -59,6 +59,8 @@ int main(){
 	// for(int i=0;i<N;i++){
 	//     scanf("%d ",&array[i]);
 	// }
+
+    klee_make_symbolic(T, sizeof(T), "T");
     klee_make_symbolic(array, 30 * sizeof(int), "array");
 
 	printf("%s\n", groupSum(0, T)==1?"YES":"NO");
