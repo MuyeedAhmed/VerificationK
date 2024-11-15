c02f7837d60cc353986def0a288b8982e56adf02
Muyeed Ahmed <akib100095@gmail.com>
2024-11-15 17:33:29 -0500

    check - server

--- LLVM_T/TestEnv/simple-tool.cpp
+++ LLVM_T/TestEnv/simple-tool.cpp
@@ -1,32 +1,31 @@
 #include "clang/AST/AST.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/FrontendAction.h"
-#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Tooling/Tooling.h"
 
 using namespace clang;
 using namespace clang::tooling;
 
-class SimpleASTVisitor : public RecursiveASTVisitor<SimpleASTVisitor> {
+class SimpleVisitor : public RecursiveASTVisitor<SimpleVisitor> {
 public:
     bool VisitFunctionDecl(FunctionDecl *FD) {
-        llvm::outs() << "Function name: " << FD->getNameAsString() << "\n";
+        llvm::outs() << "Found function: " << FD->getNameInfo().getName().getAsString() << "\n";
         return true;
     }
 };
 
-class SimpleASTConsumer : public ASTConsumer {
+class SimpleConsumer : public ASTConsumer {
 public:
     void HandleTranslationUnit(ASTContext &Context) override {
-        SimpleASTVisitor Visitor;
+        SimpleVisitor Visitor;
         Visitor.TraverseDecl(Context.getTranslationUnitDecl());
     }
 };
 
 class SimpleFrontendAction : public ASTFrontendAction {
 public:
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef) override {
-        return std::make_unique<SimpleASTConsumer>();
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &, StringRef) override {
+        return std::make_unique<SimpleConsumer>();
     }
 };
 
@@ -34,6 +33,6 @@ int main(int argc, const char **argv) {
     if (argc > 1) {
         return !runToolOnCode(std::make_unique<SimpleFrontendAction>(), argv[1]);
     }
-    llvm::errs() << "Expected input source code.\n";
+    llvm::errs() << "Usage: ./basic-tool '<code>'\n";
     return 1;
 }
