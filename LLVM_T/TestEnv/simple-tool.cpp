bd8e63b961685ae74044ee518e6c46c6e1c1168f
Muyeed Ahmed <akib100095@gmail.com>
2024-11-15 17:25:17 -0500

    check - server

--- LLVM_T/TestEnv/simple-tool.cpp
+++ LLVM_T/TestEnv/simple-tool.cpp
@@ -1,5 +1,7 @@
 #include "clang/AST/AST.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/FrontendAction.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Tooling/Tooling.h"
 
 using namespace clang;
@@ -8,7 +10,7 @@ using namespace clang::tooling;
 class SimpleASTVisitor : public RecursiveASTVisitor<SimpleASTVisitor> {
 public:
     bool VisitFunctionDecl(FunctionDecl *FD) {
-        llvm::outs() << "Function: " << FD->getNameInfo().getName().getAsString() << "\n";
+        llvm::outs() << "Function name: " << FD->getNameAsString() << "\n";
         return true;
     }
 };
@@ -23,13 +25,15 @@ public:
 
 class SimpleFrontendAction : public ASTFrontendAction {
 public:
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef File) override {
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef) override {
         return std::make_unique<SimpleASTConsumer>();
     }
 };
 
 int main(int argc, const char **argv) {
-    if (argc > 1)
+    if (argc > 1) {
         return !runToolOnCode(std::make_unique<SimpleFrontendAction>(), argv[1]);
+    }
+    llvm::errs() << "Expected input source code.\n";
     return 1;
 }
