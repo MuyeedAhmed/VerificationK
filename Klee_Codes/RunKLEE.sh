ce5d67206ad060b2b43ed9bf495e4f1df607fbf7
Muyeed Ahmed <akib100095@gmail.com>
2024-11-15 09:17:07 -0500

    check - server

--- Klee_Codes/RunKLEE.sh
+++ Klee_Codes/RunKLEE.sh
@@ -19,3 +19,13 @@ for c_file in "$SOURCE_DIR"/*.c; do
     klee --solver-backend=z3 "$file_output_dir/$base_name.bc"
 
 done
+
+
+# clang++ -std=c++17 -o DetectScanf DetectScanf.cpp \
+#   -I/usr/lib/llvm-16/include -L/usr/lib/llvm-16/lib \
+#   -lclangTooling -lclangFrontendTool -lclangFrontend -lclangDriver \
+#   -lclangSerialization -lclangCodeGen -lclangParse -lclangSema \
+#   -lclangAnalysis -lclangRewriteFrontend -lclangRewrite -lclangEdit \
+#   -lclangAST -lclangASTMatchers -lclangLex -lclangBasic -lLLVM \
+#   -lclangSupport -lclangARCMigrate -lclangStaticAnalyzerFrontend \
+#   -lclangStaticAnalyzerCore -lclangStaticAnalyzerCheckers
